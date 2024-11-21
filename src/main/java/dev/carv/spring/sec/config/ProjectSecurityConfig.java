90249d1ae2edda1854673940a3f24a88c0ff1f14
carolusquintus <carloschars@gmail.com>
2024-11-20 21:40:53 -0600

    Replace InMemoryUserDetailsManager with JdbcUserDetailsManager config

--- src/main/java/dev/carv/spring/sec/config/ProjectSecurityConfig.java
+++ src/main/java/dev/carv/spring/sec/config/ProjectSecurityConfig.java
@@ -4,14 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -31,18 +31,8 @@ public class ProjectSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager(
-            User.builder()
-                .username("user")
-                .password("{bcrypt}$2a$12$LmkbFfLC1cDB615kJFdC3ObMKHi5u1S9/4eODBi1sMyv.XHKmr7ku")
-                .authorities("READ")
-                .build(),
-            User.builder()
-                .username("admin")
-                .password("{bcrypt}$2a$12$J37YWF53955k9D98RSHL8.a9HUJi.a293kYAOH1aTcWYry7CUIWXO")
-                .authorities("ADMIN")
-                .build());
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
