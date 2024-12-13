3b7865a09e87298d0b3d239b23be82c3fbcec4e8
carolusquintus <carloschars@gmail.com>
2024-12-13 17:26:23 -0600

    Add @Profile("default") for SecurityConfig

--- src/main/java/dev/carv/spring/sec/config/SecurityConfig.java
+++ src/main/java/dev/carv/spring/sec/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package dev.carv.spring.sec.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -13,11 +14,13 @@ import org.springframework.security.web.authentication.password.HaveIBeenPwnedRe
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
+@Profile("default")
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .requiresChannel(channel -> channel.anyRequest().requiresInsecure())
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(requests -> requests
             .requestMatchers("/account", "/balance", "/card", "/loan").authenticated()
