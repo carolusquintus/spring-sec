2b7f3cb8d590cb625f787a5e43da981f5e6f8ccf
carolusquintus <carloschars@gmail.com>
2025-03-22 19:51:07 -0600

    @Repository for AccountRepository

--- src/main/java/dev/carv/spring/sec/repository/AccountRepository.java
+++ src/main/java/dev/carv/spring/sec/repository/AccountRepository.java
@@ -2,9 +2,11 @@ package dev.carv.spring.sec.repository;
 
 import dev.carv.spring.sec.model.Account;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AccountRepository extends CrudRepository<Account, Long> {
 
     Optional<Account> findByCustomerId(Long customerId);
