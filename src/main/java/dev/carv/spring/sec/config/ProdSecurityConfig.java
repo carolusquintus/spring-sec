e466077174b6134ce6e2aa6b6859f951105594ca
carolusquintus <carloschars@gmail.com>
2025-05-01 23:10:24 -0600

    PROD profile update config

--- src/main/java/dev/carv/spring/sec/config/ProdSecurityConfig.java
+++ src/main/java/dev/carv/spring/sec/config/ProdSecurityConfig.java
@@ -2,16 +2,21 @@ package dev.carv.spring.sec.config;
 
 import dev.carv.spring.sec.exception.handler.CustomAccessDeniedHandler;
 import dev.carv.spring.sec.exception.handler.CustomBasicAuthenticationEntryPoint;
+import dev.carv.spring.sec.filter.CsrfCookieFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.password.CompromisedPasswordChecker;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+
+import static org.springframework.security.config.http.SessionCreationPolicy.ALWAYS;
 
 @Configuration
 @Profile("prod")
@@ -19,11 +24,17 @@ public class ProdSecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        var csrfTokenHandler = new CsrfTokenRequestAttributeHandler();
         return http
-            .sessionManagement(session -> session.invalidSessionUrl("/invalid-session"))
+            .securityContext(secContext -> secContext.requireExplicitSave(false))
+            .sessionManagement(session -> session.sessionCreationPolicy(ALWAYS))
             .requiresChannel(channel -> channel.anyRequest().requiresSecure())
-            .csrf(AbstractHttpConfigurer::disable)
             .cors(CorsConfig::getHttpSecurityCorsConfigurer)
+            .csrf(csrfConfig -> csrfConfig
+                .ignoringRequestMatchers("/contact", "customer/sign-up")
+                .csrfTokenRequestHandler(csrfTokenHandler)
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+            .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
             .authorizeHttpRequests(requests -> requests
                 .requestMatchers("/account", "/balance", "/card", "/loan", "/customer/info").authenticated()
                 .requestMatchers("/contact", "/notice", "/error", "/customer/sign-up", "/invalid-session").permitAll())
